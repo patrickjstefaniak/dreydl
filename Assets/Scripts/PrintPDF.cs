329cb34366f4d2d0b29b12c61881c737cc79b8a8
patrickjstefaniak <patrickjstefaniak@gmail.com>
2024-04-13 19:42:05 -0700

    uggghhh printing

--- Assets/Scripts/PrintPDF.cs
+++ Assets/Scripts/PrintPDF.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Diagnostics;
+using System;
 
 public class PrintPDF : MonoBehaviour
 
 {
     
     static public string pdfFilePath = "/Users/forest/Documents/Cash_Out_Voucher_DREYDL/1.pdf";
+    //this gets changed in gamecontroller.cs 
 
     public void Print()
     {
+/*
         // Escape any double quotes in the file path
         string escapedFilePath = pdfFilePath.Replace("\"", "\\\"");
 
@@ -26,5 +29,27 @@ public class PrintPDF : MonoBehaviour
         process.Start();
         process.WaitForExit();
         process.Close();
+        */
+
+
+        string path = @"C:\Users\dreydl\Desktop\Cashout_Vouchers\"+pdfFilePath+".pdf";
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+        process.StartInfo.UseShellExecute = true;
+        process.StartInfo.FileName = path;
+        //process.StartInfo.Verb = "print";
+
+        process.Start();
+        //process.WaitForExit();
+
+
+        /*PowerShell ps = PowerShell.Create();
+        ps.AddCommand("Start-Process")
+            .AddParameter("FilePath", @"C:\Users\dreydl\Desktop\Cashout_Vouchers\{pdfFilePath}.pdf")
+            .AddParameter("Verb", "Print")
+            .Invoke();*/
+       // "Start-Process -FilePath "C:\Users\dreydl\Desktop\Cashout_Vouchers\{pdfFilePath}.pdf" -Verb Print" 
+
     }
 }
